06cf6ac0e38c2dfe2cbec53bdc5bb8de33bf57ec
Gdominguezborines <Gdominguezborines@danielcastelao.org>
2021-02-22 21:49:49 +0100

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+
+import MisMetodos.MisMetodos;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        MisMetodos.mostrar();
+
     }
 }
